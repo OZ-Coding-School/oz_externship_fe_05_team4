0ea0f64b7739801fc555f9540492b85dcc5a3430
pje8740 <pje8740@cu.ac.kr>
2026-01-03 23:58:35 +0900

    refactor: 텍트트 뷰 컴포넌트 분리(#74)
    - 텍스트에디터에서 뷰를 공통 상수로 분리

--- src/components/texteditor/TextEditor.tsx
+++ src/components/texteditor/TextEditor.tsx
@@ -1,19 +1,13 @@
 import { EditorContent } from '@tiptap/react'
 import type { Editor } from '@tiptap/core'
+import { TextView } from './TextView'
 
-type Props = {
-  editor: Editor | null
-}
+type Props = { editor: Editor | null }
 
 const TextEditor = ({ editor }: Props) => {
   if (!editor) return null
 
-  return (
-    <EditorContent
-      editor={editor}
-      className="prose max-w-none [&_.ProseMirror]:min-h-[240px] [&_.ProseMirror]:break-words [&_.ProseMirror]:whitespace-pre-wrap [&_.ProseMirror]:outline-none [&_ol]:pl-6 [&_ul]:list-disc [&_ul]:pl-6"
-    />
-  )
+  return <EditorContent editor={editor} className={TextView} />
 }
 
 export default TextEditor
